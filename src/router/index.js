149400be53f380dca4d56960d4b0b1ef22045021
Mike38 <mshar50055@gmail.com>
2020-06-29 21:50:44 +0300

    views markup done

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,54 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue")
+  },
+  {
+    path: "/categories",
+    name: "Categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/detail/:id",
+    name: "Detail",
+    meta: { layout: "main" },
+    component: () => import("../views/Detail.vue")
+  },
+  {
+    path: "/history",
+    name: "History",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue")
+  },
+  {
+    path: "/planning",
+    name: "Planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue")
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue")
+  },
+  {
+    path: "/record",
+    name: "Record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record.vue")
   }
 ];
 
