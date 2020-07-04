3f4e5690a4ed62626a06466ca9ca82cad872bae9
Mike38 <mshar50055@gmail.com>
2020-07-04 22:35:00 +0300

    pagination done

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,95 +1,53 @@
+/* eslint-disable vue/no-parsing-error */
 <template>
   <div>
-    <div class="app-main-layout">
-      <nav class="navbar orange lighten-1">
-        <div class="nav-wrapper">
-          <div class="navbar-left">
-            <a href="#">
-              <i class="material-icons black-text">dehaze</i>
-            </a>
-            <span class="black-text">12.12.12</span>
-          </div>
-
-          <ul class="right hide-on-small-and-down">
-            <li>
-              <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
-                USER NAME
-                <i class="material-icons right">arrow_drop_down</i>
-              </a>
-
-              <ul id='dropdown' class='dropdown-content'>
-                <li>
-                  <a href="#" class="black-text">
-                    <i class="material-icons">account_circle</i>Профиль
-                  </a>
-                </li>
-                <li class="divider" tabindex="-1"></li>
-                <li>
-                  <a href="#" class="black-text">
-                    <i class="material-icons">assignment_return</i>Выйти
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <ul class="sidenav app-sidenav open">
-        <li>
-          <a href="#" class="waves-effect waves-orange pointer">Счет</a>
-        </li>
-        <li>
-          <a href="#" class="waves-effect waves-orange pointer">История</a>
-        </li>
-        <li>
-          <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-        </li>
-        <li>
-          <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-        </li>
-        <li>
-          <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-        </li>
-      </ul>
-
-      <main class="app-content">
-        <div class="app-page">
-
-
-          <div>
-            <div>
-              <div class="breadcrumb-wrap">
-                <a href="/history" class="breadcrumb">История</a>
-                <a class="breadcrumb">
-                  Расход
-                </a>
-              </div>
-              <div class="row">
-                <div class="col s12 m6">
-                  <div class="card red">
-                    <div class="card-content white-text">
-                      <p>Описание:</p>
-                      <p>Сумма:</p>
-                      <p>Категория:</p>
-
-                      <small>12.12.12</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-        </div>
-      </main>
-
-      <div class="fixed-action-btn">
-        <a class="btn-floating btn-large blue" href="#">
-          <i class="large material-icons">add</i>
+    <Loader v-if="loading" />
+    <div v-else-if="record">
+      <div class="breadcrumb-wrap">
+        <router-link to="/history" class="breadcrumb">История</router-link>
+        <a @click.prevent class="breadcrumb">
+          {{ record.type === "income" ? "Доход" : "Расход" }}
         </a>
       </div>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card" :class="{
+            'red': record.type === 'outcome',
+            'green': record.type === 'income'
+          }">
+            <div class="card-content white-text">
+              <p>Описание: {{record.description}} </p>
+              <p>Сумма: {{record.amount | currency}} </p>
+              <p>Категория: {{record.categoryName}} </p>
+
+              <small>{{record.date | date('datetime')}}</small>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <p class="center" v-else>Запись с id={{$route.params.id}} не найдена</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Detail",
+  data: () => ({
+    record: null,
+    loading: true
+  }),
+  async mounted() {
+    const id = this.$route.params.id
+    const record = await this.$store.dispatch("fetchRecordById", id)
+    const category = await this.$store.dispatch("fetchCategoryById", record.categoryId)
+
+    this.record = {
+      ...record,
+      categoryName: category.title
+    }
+
+    this.loading = false
+  }
+}
+</script>
