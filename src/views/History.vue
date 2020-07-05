8702947d27dc37419845b4293265f3c863a7847d
Mike38 <mshar50055@gmail.com>
2020-07-05 21:23:58 +0300

    localization added

--- src/views/History.vue
+++ src/views/History.vue
@@ -26,7 +26,7 @@
         :next-text="'Вперед'"
         :container-class="'pagination'"
         :page-class="'waves-effect'"
-      /> 
+      />
     </section>
   </div>
 </template>
@@ -34,10 +34,10 @@
 <script>
 import paginationMixin from "../mixins/pagination.mixin";
 import HistoryTable from "../components/HistoryTable";
-import {Pie} from "vue-chartjs";
+import { Pie } from "vue-chartjs";
 
 export default {
-  name: 'history',
+  name: "History",
   extends: Pie,
   mixins: [paginationMixin],
   data: () => ({
@@ -45,59 +45,64 @@ export default {
     records: []
   }),
   async mounted() {
-    this.records = await this.$store.dispatch("fetchRecords")
-    const categoires = await this.$store.dispatch("fetchCategories")
+    this.records = await this.$store.dispatch("fetchRecords");
+    const categoires = await this.$store.dispatch("fetchCategories");
 
-    this.setup(categoires)
-  
-    this.loading = false
+    this.setup(categoires);
+
+    this.loading = false;
   },
   methods: {
     setup(categoires) {
-      this.setupPagination(this.records.map(record => {
-        return {
-          ...record,
-          categoryName: categoires.find(c => c.id === record.categoryId).title,
-          typeClass: record.type === 'income' ? 'green' : 'red',
-          typeText: record.type === 'income' ? 'Доход' : 'Расход',
-        }
-      }))
+      this.setupPagination(
+        this.records.map(record => {
+          return {
+            ...record,
+            categoryName: categoires.find(c => c.id === record.categoryId)
+              .title,
+            typeClass: record.type === "income" ? "green" : "red",
+            typeText: record.type === "income" ? "Доход" : "Расход"
+          };
+        })
+      );
 
       this.renderChart({
         labels: categoires.map(c => c.title),
-        datasets: [{
-            label: 'Расходы по категориям',
+        datasets: [
+          {
+            label: "Расходы по категориям",
             data: categoires.map(c => {
               return this.records.reduce((total, r) => {
-                if (r.categoryId === c.id && r.type === 'outcome') {
-                  total += +r.amount
+                if (r.categoryId === c.id && r.type === "outcome") {
+                  total += +r.amount;
                 }
-                return total
-              }, 0)
+                return total;
+              }, 0);
             }),
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
             ],
             borderWidth: 1
-        }]
-      })
+          }
+        ]
+      });
     }
   },
   components: {
     HistoryTable
   }
-}
+};
 </script>
